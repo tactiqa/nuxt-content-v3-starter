ac6a2566ee0851050e3cea0b8bb3b51429790a4b
jayjake <subkubli@tuta.io>
2025-06-08 13:42:31 +0200

    + some color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="min-h-screen flex flex-col">
+  <div class="min-h-screen flex flex-col relative">
+    <!-- Gradient Background -->
+    <div class="fixed inset-0 -z-10 bg-gradient-to-br from-white via-blue-50 to-blue-100"></div>
     <Header />
     <main class="flex-grow">
       <div class="container mx-auto px-4 py-8 w-full max-w-[70%]">
