9ba16169cbdbbfa91dab7d080de6cd48b42f21ef
jayjake <subkubli@tuta.io>
2025-06-08 16:34:32 +0200

    sidebar width fix + new layout

---
+++ layouts/testplan.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen flex flex-col relative">
+    <!-- Gradient Background -->
+    <div class="fixed inset-0 -z-10 bg-gradient-to-br from-white via-blue-50 to-blue-100"></div>
+    <Header />
+    <main class="flex-grow">
+      <div class="container mx-auto px-4 py-8 w-full max-w-[70%]">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+// This will be automatically imported by Nuxt 3
+</script>
